e4405f7e021be2b47167abdd93f134a7c39b4824
juanjose-gonzalez-apex <jjose@apexfintechsolutions.com>
2025-12-25 09:51:05 -0500

    redirection from http to https

--- next.config.js
+++ next.config.js
@@ -115,6 +115,10 @@ const nextConfig = {
             key: "Referrer-Policy",
             value: "origin-when-cross-origin",
           },
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=31536000; includeSubDomains",
+          },
         ],
       },
       // Cache estático agresivo para assets
